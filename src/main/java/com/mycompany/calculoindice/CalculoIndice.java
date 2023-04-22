7adbe0544100a2e048000e7a316111ef02a1c2de
juliano-ss <jotaliano@live.com>
2023-04-22 20:29:59 -0300

    adding Scanner

--- src/main/java/com/mycompany/calculoindice/CalculoIndice.java
+++ src/main/java/com/mycompany/calculoindice/CalculoIndice.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.calculoindice;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,18 +10,17 @@ package com.mycompany.calculoindice;
 public class CalculoIndice {
 
     public static void main(String[] args) {
-       
-        int INDICE = 13;
+        Scanner leitor = new Scanner(System.in);
+
+        int INDICE = leitor.nextInt();
         int SOMA = 0;
         int K = 0;
 
-       
         while (K < INDICE) {
             K = K + 1;
             SOMA = SOMA + K;
         }
 
-       
         System.out.println(SOMA);
     }
 }
