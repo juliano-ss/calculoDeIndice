9c612857a615cd08c38753a499c82238d64b6b4b
juliano-ss <jotaliano@live.com>
2023-04-22 20:36:56 -0300

    treating the variables

--- src/main/java/com/mycompany/calculoindice/CalculoIndice.java
+++ src/main/java/com/mycompany/calculoindice/CalculoIndice.java
@@ -12,7 +12,18 @@ public class CalculoIndice {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        int INDICE = leitor.nextInt();
+        int INDICE;
+        while (true) {
+            System.out.print("Digite o valor do índice (deve ser um inteiro): ");
+            if (leitor.hasNextInt()) {
+                INDICE = leitor.nextInt();
+                break;
+            } else {
+                System.out.println("Valor inválido, tente novamente.");
+                leitor.next();
+            }
+        }
+
         int SOMA = 0;
         int K = 0;
 
@@ -21,6 +32,6 @@ public class CalculoIndice {
             SOMA = SOMA + K;
         }
 
-        System.out.println(SOMA);
+        System.out.println("A soma dos " + INDICE + " primeiros números inteiros é: " + SOMA);
     }
 }
